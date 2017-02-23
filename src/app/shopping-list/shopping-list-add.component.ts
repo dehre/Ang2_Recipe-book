e6972819129c8cdfb2261b44f41b69cdf5e76735
Loris <loris4526@gmail.com>
2017-02-23 09:54:09 +0100

    Start wire up editing shopping-list items

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { ShoppingListService } from "./shopping-list.service";
+import { Ingredient } from "../shared/ingredient";
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent {
+export class ShoppingListAddComponent implements OnChanges {
+
+  @Input() item: Ingredient;
+  isAdd = true;
+
+  constructor(private sls:ShoppingListService){}
+
+  ngOnChanges(changes){
+    if(changes.item.currentValue === null){
+      this.isAdd = true;
+      this.item = {name:null,amount:null};
+    } else {
+      this.isAdd = false
+    }
+  }
+
+  onSubmit(ingredient:Ingredient){
+    if(!this.isAdd){
+      //EDIT
+    } else {
+      this.item = new Ingredient(ingredient.name,ingredient.amount);
+      this.sls.addItem(this.item);
+    }
+  }
 
 }
